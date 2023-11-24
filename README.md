d061827b0d1d057e60d94e9845c92c5c8a73cba1
Aditya Aparadh <aditya.aparadh.0@gmail.com>
2023-11-24 17:34:40 +0530

    Added all files

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Ambience
+
+Ambience is a Python script for Linux desktops that creates a seamless ambient music experience. It plays ambient music when no other media is detected, automatically fading the music when other media starts playing.
+
+Under the hood, it spawns a VLC subprocess and plays music randomly from your selected directory. When any other media is detected to be playing at more than 0% volume, the music automatically fades to a minimum or pauses playing depending on your preference.
+## Prerequisites
+1. Python 3 or above 
+2. Make sure your system uses PulseAudio server
+3. Before using Ambience, ensure you have the following packages installed:
+- vlc
+- cvlc
+- jq
+- playerctl
+- pacmd
+- pactl
+
+You can install these packages using your system's package manager. The last two are installed by default if your system uses PulseAudio server.
+
+## Configuration
+
+Ambience allows users to customize the behavior according to their preferences through the `config.json` file. Here's the default configuration file:
+
+```json
+{
+    "path": "$HOME/Music/",
+    "peak_volume": 0.4,
+    "min_volume": 0,
+    "formats": ["flac", "mp3", "ogg", "wav", "m4a"],
+    "check_interval": 1,
+    "fade_duration": 3,
+    "fade_steps": 20
+}
+```
+
+## Usage
+
+
+1. Clone this repository into a suitable directory:
+ ```
+git clone https://github.com/AdityaAparadh/Ambience
+```
+
+2. Navigate to Ambience directory:
+```
+cd Ambience
+```
+
+3. Adjust `config.json` according to your preferences. Make sure `"path"` points to the directory where your music is stored.
+
+  
+4. Run the `main.py` script:
+```
+python main.py
+```
+    OR
+
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,69 @@
+import subprocess
+import time
+import json
+import os
+
+def is_media_playing():
+    try:
+        result = subprocess.run(['pacmd', 'list-sink-inputs'], capture_output=True, text=True, check=True)
+        streams = result.stdout.split('index:')
+        for stream in streams:
+            if 'state: RUNNING' in stream and 'VLC media player' not in stream:
+                return True
+        return False
+    except subprocess.CalledProcessError:
+        return False
+
+def play_ambient_music():
+    global player
+    player = subprocess.Popen(['cvlc', 'playlist.m3u', '--random', '--loop', '--gain', str(config['peak_volume'])])
+
+global controller
+controller = subprocess.Popen('echo', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+def Control(instruction):
+    subprocess.run(['playerctl','--player=vlc', instruction])
+
+def Fade(target, interval, steps):
+    current_volume = subprocess.check_output(['playerctl', '--player=vlc', 'volume'])
+    current_volume = float(current_volume.strip())
+    step = (target - current_volume) / steps
+    for _ in range(steps):
+        current_volume += step
+        current_volume = max(0, min(1, current_volume))
+        subprocess.run(['playerctl', '--player=vlc', 'volume', str(current_volume)])
+        time.sleep(interval/steps)
+
+
+def main():
+    with open('config.json', 'r') as f:
+        global config
+        config = json.load(f)    
+    DELAY = config['check_interval']
+    print("DELAY : ", DELAY)
+    subprocess.run(['./playlist.sh'], shell=True)
+
+    ambient_music_playing = False
+    play_ambient_music()
+    while True:
+        time.sleep(DELAY)
+        os.system("clear")
+        media = is_media_playing()
+        print("External Media Playing: " + str(media))
+        current_volume = subprocess.check_output(['playerctl', '--player=vlc', 'volume'])
+        current_volume = float(current_volume.strip())
+        formatted_volume = "{:.2f}%".format(current_volume * 100)
+        print("Volume : " + str(formatted_volume))
+        
+        if media and ambient_music_playing:
+            Fade(config['min_volume'], config['fade_duration'], config['fade_steps'])
+            if(config['min_volume'] == 0):
+                Control('pause')
+                ambient_music_playing = False
+        elif not media :
+            Control('play')
+            Fade(config['peak_volume'], config['fade_duration'], config['fade_steps'])
+            ambient_music_playing = True
+
+if __name__ == "__main__":
+        main()
